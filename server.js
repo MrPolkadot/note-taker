208ab661662ea0c1fba41143d77b10534a7217da
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-09-02 12:02:20 -0600

    added Delete method to delete notes

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
+//Imports packages and modules
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
-const { log } = require("console");
 const notesDB = require("./db/db.json");
+
+//Retrieves all of express.js methods for use
 const app = express();
 
+
+//Creates a port to deploy our server
 const PORT = process.env.PORT || 2099;
 
 
-console.log("starting");
+console.log("Starting");
 
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 
+//Add comment
+const noteData = fs.readFileSync("./db/db.json", "utf-8", (err, data) => {
+    if (err) {
+        console.log(err);
+    } else return data;
+
+});
+const notes = JSON.parse(noteData);
 
 
-//WORK ON THIS!
+//Add comment
 app.get("/api/notes", (req, res) => {
     fs.readFile("./db/db.json", "utf-8", (err, data) => {
         if (err) {
@@ -29,50 +41,33 @@ app.get("/api/notes", (req, res) => {
             res.json(notes);
         }
     })
-
-
 });
 
-const noteData = fs.readFileSync("./db/db.json", "utf-8", (err, data) => {
-    if (err) {
-        console.log(err);
-    } else return data;
 
-});
-const notes = JSON.parse(noteData);
+
+//Add comment
 app.post("/api/notes", (req, res) => {
-    console.log(req.body);
-
-    // const noteData = fs.readFileSync("./db/db.json", "utf-8", (err, data) => {
-    //     if (err) {
-    //         console.log(err);
-    //     } else return data;
-
-    // });
-    // const notes = JSON.parse(noteData);
-
     const { title, text } = req.body;
-
     if (req.body) {
         const newNote = {
             title,
             text,
             note_id: uuidv4()
-
         };
-        notes.push(newNote);
 
+        notes.push(newNote);
 
         fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, "\t"), err => {
             if (err) throw err;
             //return true;
         });
         res.json(notes);
-
     }
 
 })
 
+
+//
 app.get("/api/notes/:note_id", (req, res) => {
     if (req.params.note_id) {
         const noteId = req.params.note_id;
@@ -87,31 +82,37 @@ app.get("/api/notes/:note_id", (req, res) => {
     } else {
         res.status(404).send("Note id not provided.")
     }
-
-    //res.json(notes[req.params.id]);
-
 })
 
+
+//
 app.delete("/api/notes/:id", (req, res) => {
     const id = req.params.id * 1;
     const deleteNote = notes.find(el => el.id === id);
-    const index = movies.indexOf(deleteNote);
+    const index = notes.indexOf(deleteNote);
 
     notes.splice(index, 1);
     fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, "\t"), err => {
         if (err) throw err;
         //return true;
     });
+    res.json(notes);
 })
 
+
+//
 app.get("/notes", (req, res) =>
     res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
 
+
+//
 app.get("*", (req, res) =>
     res.sendFile(path.join(__dirname, "public/index.html"))
 );
 
 
+
+//
 app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
