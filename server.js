89503f3c908c25abaf5b7b94d086c708eb5df492
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-09-02 09:58:53 -0600

    Created get method to get notes from its id.

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const path = require("path");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 const { log } = require("console");
+const notesDB = require("./db/db.json");
 const app = express();
 
-const PORT = 2099;
+const PORT = process.env.PORT || 2099;
 
 
 console.log("starting");
@@ -32,17 +33,23 @@ app.get("/api/notes", (req, res) => {
 
 });
 
+const noteData = fs.readFileSync("./db/db.json", "utf-8", (err, data) => {
+    if (err) {
+        console.log(err);
+    } else return data;
 
+});
+const notes = JSON.parse(noteData);
 app.post("/api/notes", (req, res) => {
     console.log(req.body);
 
-    const noteData = fs.readFileSync("./db/db.json", "utf-8", (err, data) => {
-        if (err) {
-            console.log(err);
-        } else return data;
+    // const noteData = fs.readFileSync("./db/db.json", "utf-8", (err, data) => {
+    //     if (err) {
+    //         console.log(err);
+    //     } else return data;
 
-    });
-    const notes = JSON.parse(noteData);
+    // });
+    // const notes = JSON.parse(noteData);
 
     const { title, text } = req.body;
 
@@ -66,6 +73,36 @@ app.post("/api/notes", (req, res) => {
 
 })
 
+app.get("/api/notes/:note_id", (req, res) => {
+    if (req.params.note_id) {
+        const noteId = req.params.note_id;
+        for (let i = 0; i < notesDB.length; i++) {
+            const currentNote = notesDB[i];
+            if (currentNote.note_id === noteId) {
+                res.json(currentNote);
+                return;
+            }
+        }
+        res.status(404).send("Note not found.")
+    } else {
+        res.status(404).send("Note id not provided.")
+    }
+
+    //res.json(notes[req.params.id]);
+
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+    const id = req.params.id * 1;
+    const deleteNote = notes.find(el => el.id === id);
+    const index = movies.indexOf(deleteNote);
+
+    notes.splice(index, 1);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, "\t"), err => {
+        if (err) throw err;
+        //return true;
+    });
+})
 
 app.get("/notes", (req, res) =>
     res.sendFile(path.join(__dirname, "public/notes.html"))
